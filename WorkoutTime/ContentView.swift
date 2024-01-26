227eef21f8087a77fa2fdd03cd64860c13702b49
jrderanian <105163216+jrderanian@users.noreply.github.com>
2024-01-25 22:54:58 -0600

    Initial Commit

---
+++ WorkoutTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutTime
+//
+//  Created by John Deranian on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutTime/WorkoutTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimeApp.swift
+//  WorkoutTime
+//
+//  Created by John Deranian on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
