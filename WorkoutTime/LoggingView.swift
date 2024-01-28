edd632086a6d5c17b70bc010d68dc21b0e049cc3
jrderanian <105163216+jrderanian@users.noreply.github.com>
2024-01-27 21:45:16 -0600

    I spent hours trying to use a Dictionary, but failed.  so created and ActivityLog class and ActivityLogs that will be logged.  The toggle in LoggingView took hours to get to work.  Why? Because Dictionary is horrible.  By create the addtional ActivityLog(s) and find how to bind to values inside an array solved the problem.  Without this: https://www.swiftbysundell.com/articles/bindable-swiftui-list-elements/ I would have never gotten it to work. (the right way)

---
+++ WorkoutTime/LoggingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoggingView.swift
+//  WorkoutTime
+//
+//  Created by John Deranian on 1/27/24.
+//
+
+import SwiftUI
+
+struct LoggingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoggingView()
+}
